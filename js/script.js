9e0fc881c288a2d55ec0020c0f07c59a772958a9
Julie Young <student@macadmicbook165.dalton.org>
2021-06-14 09:40:01 -0400

    scoreboard draft

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/* script.js 
+   Author:
+   Date:
+*/
+
+
+$(document).ready(function(){ // begin document.ready block
+
+	// ONE
+
+
+
+
+	// TWO
+
+
+
+
+	// THREE
+
+
+
+
+	// FOUR 
+
+
+
+
+	// FIVE
+
+
+	
+
+
+
+}); //end document.ready block
